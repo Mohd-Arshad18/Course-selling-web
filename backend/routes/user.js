8257b57da89a603bd050568a752a78d2e1ef0d19
Mohd Arshad <mohdarshad1034@gmail.com>
2025-07-24 13:04:05 +0530

    Create user.js

---
+++ backend/routes/user.js
@@ -0,0 +1,86 @@
+const { Router } = require('express');
+const { userModel, purchaseModel, courseModel } = require("../db");
+const jwt = require('jsonwebtoken');
+const { JWT_USER_PASSWORD } = require("../config");
+const { userMiddleware } = require("../middleware/user");
+const bcrypt = require('bcrypt');
+
+const userRouter = Router();
+const saltRound = 10;
+
+userRouter.post("/signup", async function(req, res){
+    try{
+        const { email, password, firstname, lastname} = req.body;
+        
+        if (!email || !password || !firstname || !lastname) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+        
+        const existingUser = await userModel.findOne({ email });
+        if (existingUser) {
+            return res.status(409).json({ message: "User already exists" });
+        }
+    
+        const hashedPassword = await bcrypt.hash(password, saltRound);
+        
+        await userModel.create({
+        email: email,
+        password: hashedPassword,
+        firstname: firstname,
+        lastname: lastname
+        })
+    
+        res.json({
+        message: "Signup succeeded"
+        })
+    } catch(e){
+        res.status(500).json({
+            message: "Internal error"
+        })
+    }    
+})
+
+userRouter.post("/signin", async function(req, res){
+    const { email, password } = req.body;
+    
+    const user = await userModel.findOne({ email });
+
+    if (user && await bcrypt.compare(password, user.password)) {
+        const token = jwt.sign({ id: user._id }, JWT_USER_PASSWORD);
+        res.json({
+            message: "You are signed in",
+            token
+        });
+    } else {
+        res.status(403).json({
+            message: "Incorrect credentials"
+        });
+    }
+});
+
+userRouter.get("/purchases", userMiddleware, async function (req, res){
+    const userId = req.userId;
+    
+    const purchases = await purchaseModel.find({
+        userId,
+    });
+    
+    let purchaseCourseIds = [];
+    
+    for(let i = 0; i<purchases.length ; i++){
+        purchaseCourseIds.push(purchases[i].courseId)
+    }
+    
+    const courseData = await courseModel.find({
+        _id: { $in: purchaseCourseIds }
+    })
+    
+    res.json({
+        purchases,
+        courseData
+    })
+})
+
+module.exports = {
+    userRouter: userRouter
+}
